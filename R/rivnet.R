5f4b556614ea68dba893e4fc63c95881ac4bc043
aarkilanian <a.arkilanian@gmail.com>
2022-02-03 12:29:02 -0500

    feat: create internal rivnet constructor

--- R/rivnet.R
+++ R/rivnet.R
@@ -9,6 +9,54 @@ new_rivnet <- function(rivers,
                        snap = TRUE,
                        snap.tolerance = 100){
 
+  # Assign 0% permeability to barriers by default if other permeability is not supplied
+  if(is.null(bar.perm)){
+    barriers$perm <- 0
+
+    # If barrier permeabilities are supplied attempt to add them to the barriers
+  } else {
+    # Check validity of barrier permeability
+    if(length(bar.perm) != nrow(barriers)){
+      stop("Length of barrier permeabilities is not equal to the number of barriers")
+    }
+    # Convert barrier permeabilities to double type
+    bar.perm <- tryCatch(
+      as.double(bar.perm),
+      error = function(e){
+        stop("Cannot coerce barrier permeabilities to double", call. = FALSE)
+      }
+    )
+    # Assign permeabilities to barriers
+    barriers$perm <- bar.perm
+  }
+
+  # If specified, perform dendritic topology check of rivers
+  if(topology.check){
+    rivers <- enforce_dendritic(rivers)
+  }
+
+  # If supplied combine barriers and extra points
+  if(!is.null(extra.pts)){
+    # Add 100% permeability to extra points
+    extra.pts$perm <- 1
+    nodes <- sf::st_combine(barriers, extra.pts)
+  }
+
+  # If supplied combine sinks to nodes
+  if(!is.null(sinks)){
+    sinks$perm <- 1
+    nodes <- sf::st_combine(nodes, sinks)
+  }
+
+  # If specified, snap nodes to river edges
+  if(snap){
+    nodes <- sf::st_snap(nodes, rivers, snap.tolerance)
+  }
+
+  # Combine nodes and edges into sfnetwork object
+  sfnet <- sfnetworks::sfnetwork(nodes = nodes, edges = rivers, ...)
+  rivnet <- structure(sfnet, class = c("rivnet", class(sfnet)))
+
 }
 
 validate_rivnet <- function(){
