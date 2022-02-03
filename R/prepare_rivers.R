374aebb4cc2e5dce25c196736630c3749abd3b4e
aarkilanian <a.arkilanian@gmail.com>
2022-02-03 16:30:34 -0500

    feat: use sfnetwork nodes to ensure rivers split at confluences

--- R/prepare_rivers.R
+++ R/prepare_rivers.R
@@ -1,5 +1,17 @@
 prepare_rivers <- function(rivers){
 
+  # Remove Z/M dimension
+  rivers <- sf::st_zm(rivers)
 
+  # Convert to linestring geometries only
+  rivers <- st_cast(rivers, "LINESTRING")
+
+  # Build initial network
+  river_net <- sfnetworks::as_sfnetwork(rivers)
+
+  # Split rivers at node locations
+  nodes <- river_net %>% sfnetworks::activate(nodes) %>% sf::st_as_sf()
+  nodes <- sf::st_combine(nodes)
+  rivers <- sf::st_collection_extract(lwgeom::st_split(rivers, nodes), "LINESTRING")
 
 }
