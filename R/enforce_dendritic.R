c6d70f34f5b6f4e088776fab4eb127734d5a1860
aarkilanian <a.arkilanian@gmail.com>
2022-02-03 16:31:14 -0500

    feat: initial dendritic correction function

--- R/enforce_dendritic.R
+++ R/enforce_dendritic.R
@@ -1,3 +1,14 @@
 enforce_dendritic <- function(rivers){
 
+  # Prepare sfnetwork object
+  river_net <- sfnetworks::as_sfnetwork(rivers)
+
+  # Identify diverging streams
+  div_riv <- river_net %>%
+    sfnetworks::activate(edges) %>%
+    data.frame() %>%
+    dplyr::mutate(from_grp = forcats::fct_lump_min(as.factor(from), min = 2)) %>%
+    dplyr::filter(from_grp != "Other") %>%
+    dplyr::select(rivID, from)
+
 }
