6e4c3e86c7fbecec9549b5ab5f25c1832c2f87b0
aarkilanian <a.arkilanian@gmail.com>
2022-02-07 12:03:02 -0500

    test: updated river preparation tests

--- tests/testthat/test-prepare-rivers.R
+++ tests/testthat/test-prepare-rivers.R
@@ -4,9 +4,9 @@ test_riv <- st_sfc(st_linestring(matrix(c(1,3,1,3), 2)),
                    st_linestring(matrix(c(1,2,3,2), 2)))
 
 test_that("number of extracted nodes matches confluence and sources", {
-  expect_equal(extract_nodes(test_riv), 6)
+  expect_equal(length(sf::st_cast(extract_nodes(test_riv), "POINT")), 6)
 })
 
 test_that("number of split rivers matches expectation", {
-  expect_equal(nrow(prepare_rivers(test_riv)), 5)
+  expect_equal(length(prepare_rivers(test_riv)), 5)
 })
