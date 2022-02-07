d0c9366d4db6b9c563a33279df555ffea35e6390
aarkilanian <a.arkilanian@gmail.com>
2022-02-07 11:58:40 -0500

    refactor: moved node extraction of river preparation to own function

--- R/prepare_rivers.R
+++ R/prepare_rivers.R
@@ -1,17 +1,13 @@
-prepare_rivers <- function(rivers, weighting){
+prepare_rivers <- function(rivers, weighting = NULL){
 
   # Remove Z/M dimension
   rivers <- sf::st_zm(rivers)
-
   # Convert to linestring geometries only
   rivers <- st_cast(rivers, "LINESTRING")
 
-  # Build initial network
-  river_net <- sfnetworks::as_sfnetwork(rivers)
-
-  # Split rivers at node locations
-  nodes <- river_net %>% sfnetworks::activate(nodes) %>% sf::st_as_sf()
-  nodes <- sf::st_combine(nodes)
+  # Extract nodes at ends of lines and confluences
+  nodes <- extract_nodes(rivers)
+  # Split rivers according to node locations
   rivers_prep <- sf::st_collection_extract(lwgeom::st_split(rivers, nodes), "LINESTRING")
 
   # Re-incorporate weighting if specified
@@ -20,5 +16,19 @@ prepare_rivers <- function(rivers, weighting){
   }
 
   invisible(rivers_prep)
+
+}
+
+extract_nodes <- function(rivers){
+
+  # Create sfnetwork object
+  river_net <- sfnetworks::as_sfnetwork(rivers)
+
+  # Extract nodes
+  nodes <- sf::st_as_sf(sfnetworks::activate(river_net, nodes))
+  nodes <- sf::st_combine(nodes)
+
+  # Return nodes
+  invisible(nodes)
 
 }
