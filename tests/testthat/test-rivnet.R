059d453e3bfdd46456d9d3eea5509295780a203b
aarkilanian <a.arkilanian@gmail.com>
2022-02-07 14:19:39 -0500

    refactor: renamed test file and removed unnecessary test

--- tests/testthat/test-rivnet.R
+++ tests/testthat/test-rivnet.R
@@ -3,10 +3,6 @@ test_riv <- st_sfc(st_linestring(matrix(c(1,3,1,3), 2)),
                    st_linestring(matrix(c(3,4,3,3), 2)),
                    st_linestring(matrix(c(1,2,3,2), 2)))
 
-test_that("number of extracted nodes matches confluence and sources", {
-  expect_equal(length(sf::st_cast(extract_nodes(test_riv), "POINT")), 6)
-})
-
-test_that("number of split rivers matches expectation", {
+test_that("rivers are split correctly in river preparation", {
   expect_equal(length(prepare_rivers(test_riv)), 5)
 })
