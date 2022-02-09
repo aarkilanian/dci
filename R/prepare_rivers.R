5b1c316581c70daeb5078405a8b93cce2d64ce5c
aarkilanian <a.arkilanian@gmail.com>
2022-02-09 16:46:44 -0500

    bug: rivers extracted from sfnetwork

--- R/prepare_rivers.R
+++ R/prepare_rivers.R
@@ -5,6 +5,7 @@ prepare_rivers <- function(river_net){
   nodes <- sf::st_combine(nodes)
 
   # Split rivers according to node locations
+  rivers <- sf::st_as_sf(sfnetworks::activate(river_net, edges))
   rivers_prep <- sf::st_collection_extract(lwgeom::st_split(rivers, nodes), "LINESTRING")
 
   invisible(rivers_prep)
