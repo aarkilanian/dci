3abe09ec5827bbdf5ffd0445609391c4cf458d69
aarkilanian <a.arkilanian@gmail.com>
2022-02-09 19:22:30 -0500

    feat: skip when no divergence

--- R/enforce_dendritic.R
+++ R/enforce_dendritic.R
@@ -4,8 +4,16 @@ enforce_dendritic <- function(river_net){
   div_riv <- river_net %>%
     sfnetworks::activate(edges) %>%
     data.frame() %>%
+    dplyr::mutate(rivID = 1:dplyr::n()) %>%
     dplyr::mutate(from_grp = forcats::fct_lump_min(as.factor(from), min = 2)) %>%
     dplyr::filter(from_grp != "Other") %>%
     dplyr::select(rivID, from)
+
+  # Skip correction if no divergences are found
+  if(nrow(div.riv) = 0){
+    # Issue message to user if no divergences found
+    message("No divergences found.")
+    break()
+  }
 
 }
