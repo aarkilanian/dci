f78aaa4745cdaf991c08abdaa54737af5bf6b0ef
Alex <a.arkilanian@gmail.com>
2022-02-15 16:05:01 -0500

    feat: skip topology building if rivers split correctly

--- R/resplit_rivers.R
+++ R/resplit_rivers.R
@@ -6,9 +6,22 @@ resplit_rivers <- function(river_net){
 
   # Split rivers according to node locations
   rivers <- sf::st_as_sf(sfnetworks::activate(river_net, edges))
-  rivers_prep <- sf::st_collection_extract(lwgeom::st_split(rivers, nodes), "LINESTRING")
-  rivers_prep <- rivers_prep[, !names(rivers_prep) %in% c("from" ,"to")]
+  rivers_prep <- sf::st_collection_extract(lwgeom::st_split(rivers, nodes), "LINESTRING") %>%
+    dplyr::select(-c(from,to)) %>%
+    dplyr::mutate(rivID = 1:dplyr::n()) %>%
+    sf::st_as_sf()
 
-  invisible(rivers_prep)
+  ######################################################################
+  # Need to have a method here to deal with weighting on a split segment
+  ######################################################################
 
+  # If no modifications are required, return the original network
+  if(nrow(rivers_prep) == nrow(rivers)){
+    invisible(river_net)
+    # Build and return sfnetwork object
+  } else {
+    nodes <- sf::st_cast(nodes, "POINT")
+    river_net <- sfnetworks::as_sfnetwork(rivers_prep)
+    invisible(river_net)
+  }
 }
