a8d94cf6e2d80c857437e422fd34cd19a197bd77
Alex <a.arkilanian@gmail.com>
2022-02-17 17:54:50 -0500

    feat: rivnet object point data import

--- R/rivnet.R
+++ R/rivnet.R
@@ -4,39 +4,93 @@ new_rivnet <- function(rivers,
                        barriers,
                        sinks = NULL,
                        riv.weight = NULL,
-                       bar.perm = NULL,
+                       bar_perm = NULL,
                        extra.pts = NULL,
                        output_errors = FALSE,
                        snap = TRUE,
                        snap.tolerance = 100){
 
-  # Prepare rivers
+  ### Prepare network nodes
+  #
+  ## Prepare barriers
+  #
+  # Remove Z/M dimension from barriers
+  barriers <- sf::st_zm(barriers)
+  # Assign unique ID to barriers
+  barriers$id <- 1:nrow(barriers)
+  # Store given attributes and remove them
+  barriers_old <- barriers
+  barriers <- dplyr::select(barriers, id)
+  # Assign 0% permeability to barriers by default if other permeability is not supplied
+  if(is.null(bar_perm)){
+    barriers$perm <- 0
+    # If barrier permeabilities are supplied attempt to add them to the barriers
+  } else {
+    # Convert barrier permeabilities to double type
+    bar_perm <- as.double(bar.perm)
+    # Assign permeabilities to barriers
+    barriers$perm <- bar.perm
+  }
+  # Assign a type to barrier nodes
+  barriers$nodeType <- "Barrier"
+  # Rename barriers to nodes
+  nodes <- barriers
+  #
+  ## Prepare sinks
+  #
+  # If supplied combine sinks to barriers
+  if(!is.null(sinks)){
+    # Remove Z/M dimension from sinks
+    sinks <- sf::st_zm(sinks)
+    # Assign unique ID to sinks
+    sinks$id <- 1:nrow(sinks)
+    # Store given attributes and remove them
+    sinks_old <- sinks
+    sinks <- dplyr::select(sinks, id)
+    # Add permeability of 1
+    sinks$perm <- 1
+    # Assign a type to sink nodes
+    sinks$nodeType <- "Sink"
+    # Combine sinks with existing nodes
+    nodes <- rbind(nodes, sinks)
+  }
+  #
+  ## Prepare extra points
+  #
+  if(!is.null(extra.pts)){
+    # Remove Z/M dimension from sinks
+    extra.pts <- sf::st_zm(extra.pts)
+    # Assign unique ID to points
+    extra.pts$id <- 1:nrow(extra.pts)
+    # Store given attributes and remove them
+    extra.pts_old <- extra.pts
+    extra.pts <- dplyr::select(extra.pts, id)
+    # Add permeability of 1
+    extra.pts$perm <- 1
+    # Assign a type to extra nodes
+    extra.pts$nodeType <- "Extra"
+    # Combine extra points with existing nodes
+    nodes <- rbind(nodes, extra.pts)
+  }
+
+  print(nodes)
+
+
+  ### Prepare network edges
+  #
   # Remove Z/M dimension from rivers
   rivers <- sf::st_zm(rivers)
   # Convert to rivers to linestring geometries only
   rivers <- sf::st_cast(rivers, "LINESTRING")
   # Create initial sfnetwork object and topology
-  river_net <- sfnetworks::as_sfnetwork(rivers)
+  river_net <- sfnetworks::sfnetwork(rivers)
 
   # Correct river splitting
   rivers <- resplit_rivers(river_net)
   # Correct non-dendritic topologies
   rivers <- enforce_dendritic(river_net, output_errors)
 
-  # Prepare barriers
-  barriers <- sf::st_zm(barriers)
 
-  # Assign 0% permeability to barriers by default if other permeability is not supplied
-  if(is.null(bar.perm)){
-    barriers$perm <- 0
-
-    # If barrier permeabilities are supplied attempt to add them to the barriers
-  } else {
-    # Convert barrier permeabilities to double type
-    bar.perm <- as.double(bar.perm)
-    # Assign permeabilities to barriers
-    barriers$perm <- bar.perm
-  }
 
   # If supplied combine barriers and extra points
   if(!is.null(extra.pts)){
@@ -47,11 +101,7 @@ new_rivnet <- function(rivers,
     nodes <- sf::st_combine(barriers, extra.pts)
   }
 
-  # If supplied combine sinks to nodes
-  if(!is.null(sinks)){
-    sinks$perm <- 1
-    nodes <- sf::st_combine(nodes, sinks)
-  }
+
 
   # If specified, snap nodes to river edges
   if(snap){
