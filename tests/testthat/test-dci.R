2fc14234140709f9295b499290664a5aa6d80154
Alex <a.arkilanian@gmail.com>
2022-03-25 15:18:37 -0400

    test: improved testing skeleton for dci calculation functions

--- tests/testthat/test-dci.R
+++ tests/testthat/test-dci.R
@@ -1,3 +1,15 @@
+test_that("Invalid network returns error", {
+
+})
+
+test_that("Invalid DCI form request returns error", {
+
+})
+
+test_that("Invalid sink returns error", {
+
+})
+
 test_that("Can find path to root", {
 
 })
@@ -7,5 +19,13 @@ test_that("Can find path between segments", {
 })
 
 test_that("Can gather permeabilities", {
+
+})
+
+test_that("Potamodromous DCI works", {
+
+})
+
+test_that("Diadromous DCI works", {
 
 })
