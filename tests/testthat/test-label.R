2b7356b143b3886ad3de7bfa4fb8de84c9dbe7cb
Alex <a.arkilanian@gmail.com>
2022-03-25 17:09:17 -0400

    test: high level labeling testing

--- tests/testthat/test-label.R
+++ tests/testthat/test-label.R
@@ -1,7 +1,23 @@
-test_that("Correct depth of labels created", {
+test_that("Correct node labels created", {
+
+  # Load rivnet
+  net <- readRDS(test_path("testdata", "test_net.rds"))
+
+  # Run test
+  net_label <- node_labeling(net)
+  labels <- net_label %>% sfnetworks::activate(nodes) %>% dplyr::pull(node.label)
+  expect_snapshot(labels)
 
 })
 
-test_that("Correct number of labels created", {
+test_that("Correct member labels created", {
+
+  # Load rivnet
+  net <- readRDS(test_path("testdata", "test_net.rds"))
+
+  # Run test
+  net_label <- membership_labeling(net)
+  labels <- net_label %>% sfnetworks::activate(nodes) %>% dplyr::pull(member.label)
+  expect_snapshot(labels)
 
 })
