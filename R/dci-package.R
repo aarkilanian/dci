a601699bbbab3b28637bc5431d37fec8cdafb813
Alex <a.arkilanian@gmail.com>
2022-03-30 19:41:18 -0400

    misc: added list of global variables

--- R/dci-package.R
+++ R/dci-package.R
@@ -6,3 +6,25 @@
 #' @importFrom sfnetworks activate
 ## usethis namespace: end
 NULL
+
+## quiets concerns of R CMD check re: the dplyr tidy selection
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("edges",
+                                                        "nodes",
+                                                        "from",
+                                                        "to",
+                                                        "weight",
+                                                        "member.label",
+                                                        "segweight",
+                                                        "type",
+                                                        "from_len",
+                                                        "to_len",
+                                                        "degree",
+                                                        "rivID",
+                                                        "complexID",
+                                                        "rowID",
+                                                        "n",
+                                                        "component",
+                                                        "perm",
+                                                        "id",
+                                                        "geometry",
+                                                        "group"))
