2d4b535b40238d4fdf3e3b96450cd4668a0c1710
Alex <a.arkilanian@gmail.com>
2022-06-01 18:11:31 -0400

    docs: removed and renamed some parameters, removed simplified constructor

--- R/river_net.R
+++ R/river_net.R
@@ -8,9 +8,7 @@
 #'
 #' @param sinks A \code{sinks} object returned by \code{\link{import_points}}. This data is optional.
 #'
-#' @param others A \code{others} object returned by \code{\link{import_points}}. This data is optional.
-#'
-#' @param tolerance An integer value, the maximum snapping distance in map units of supplied points relative to river lines. Points outside this distance will be discarded. Defaults to 10 map units.
+#' @param poi A \code{others} object returned by \code{\link{import_points}}. This data is optional.
 #'
 #' @param check A logical value, if \code{TRUE}, the default, dendritic topology of the river network is enforced with \code{\link{enforce_dendritic}}.
 #'
@@ -20,7 +18,7 @@
 river_net <- function(rivers,
                    barriers,
                    sinks = NULL,
-                   others = NULL,
+                   poi = NULL,
                    check = TRUE){
 
   # Check rivers
@@ -87,55 +85,5 @@ river_net <- function(rivers,
   # Define river_net class
   net <- structure(net, class = c("river_net", class(net)))
   invisible(net)
-
-}
-
-# Simplified constructor
-# For internal use only
-new_rivnet <- function(rivers,
-                       barriers,
-                       sinks = NULL,
-                       riv_weight = NULL,
-                       others = NULL,
-                       snap_tolerance = 10,
-                       correct_topology = TRUE){
-
-  # Match river projection
-  barriers <- sf::st_transform(barriers, sf::st_crs(rivers))
-  sinks <- sf::st_transform(sinks, sf::st_crs(rivers))
-  if(!is.null(others)){
-    others <- sf::st_transform(others, sf::st_crs(rivers))
-  }
-
-  # Combine nodes together
-  user_nodes <- dplyr::bind_rows(barriers, sinks, others)
-
-  # Clean up topology if requested
-  if(correct_topology == TRUE){
-    # Perform necessary corrections
-    rivers <- enforce_dendritic(rivers)
-  }
-
-  # Split rivers at user node locations
-  rivers <- split_rivers_at_points(rivers, user_nodes, snap_tolerance) %>%
-    dplyr::mutate(rivID = 1:dplyr::n())
-
-  # Create final sfnetwork
-  suppressWarnings(
-    rivnet <- sfnetworks::as_sfnetwork(rivers)
-  )
-
-  # Join special node attributes
-  rivnet <- join_attributes(rivnet, user_nodes, snap_tolerance)
-
-  # Apply binary labeling
-  rivnet <- node_labeling(rivnet)
-
-  # Apply membership labelling
-  rivnet <- membership_labeling(rivnet)
-
-  # Define rivnet class
-  rivnet <- structure(rivnet, class = c("rivnet", class(rivnet)))
-  invisible(rivnet)
 
 }
