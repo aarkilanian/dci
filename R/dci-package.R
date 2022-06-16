ae647b750fa1832531c91ab9811bca4a5ae0e258
aarkilanian <a.arkilanian@gmail.com>
2022-06-16 15:24:08 -0400

    fix: add "from" and "to" to package global variables

--- R/dci-package.R
+++ R/dci-package.R
@@ -9,4 +9,4 @@
 NULL
 
 # Quiet concerns about reserved words nodes and edges from sfnetworks package
-if(getRversion() >= "2.15.1")  utils::globalVariables(c("nodes", "edges"))
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("nodes", "edges", "from", "to"))
