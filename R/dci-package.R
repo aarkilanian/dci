527e93418260affc31b03165c0250ee6d3e39fc1
aarkilanian <a.arkilanian@gmail.com>
2025-05-13 16:09:29 -0230

    misc: small change to comment

--- R/dci-package.R
+++ R/dci-package.R
@@ -8,5 +8,5 @@
 ## usethis namespace: end
 NULL
 
-# Quiet concerns about reserved words nodes and edges from sfnetworks package
+# Quiet warnings about reserved words nodes and edges from sfnetworks package
 if(getRversion() >= "2.15.1")  utils::globalVariables(c("nodes", "edges", "from", "to"))
