3d8184a4a6f69656f711cccf8731a2028ea04bd4
aarkilanian <a.arkilanian@gmail.com>
2025-05-23 14:25:25 -0230

    docs: added documentation for new data and new data

--- R/yamaska.R
+++ R/yamaska.R
@@ -1,25 +1,76 @@
 #' River network for the Yamaska watershed
 #'
-#' A spatial dataset (a \code{\link{river_net}} object) containing a topologically dendritic representation of the rivers of the Yamaska watershed in Southern Quebec along with topological, barrier, and the outlet nodes identified.
+#' A spatial `river_net` object extending the `tidygraph` representation of
+#' spatial graphs. Nodes represent confluences, river endpoints, barriers, and
+#' the outlet. Edges represent stream reaches. Spatial features are projected to
+#' EPSG:32198.
 #'
-#' @format An object of class \code{\link{river_net}} holding a pair of \code{\link[sf]{sf}} objects representing the edges (588 \code{LINESTRING} geometries and 7 columns) and the nodes (589 \code{POINT} geometries and 7 columns) :
+#' @format A `river_net` object with:
+#'\describe{
+#'  \item{Nodes}{
+#'    \describe{
+#'      \item{geometry}{the geometry list column of the node point features}
+#'      \item{pass_1}{the passability of nodes in the network from 0 (impassable) to 1}
+#'      \item{pass_2}{the binary passability of nodes in the network, 0 if a barrier and 1 otherwise}
+#'      \item{type}{the type of the node: topological, barrier, or outlet}
+#'    }
+#'  }
+#'  \item{Edges}{
+#'    \describe{
+#'      \item{from}{the row index of the origin node of the edge feature}
+#'      \item{to}{the row index of the destination node of the edge feature}
+#'      \item{qual}{a simulated weighting based on habitat quality of features}
+#'      \item{riv_length}{length of edge features in meters}
+#'      \item{rivID}{unique river feature integer ID}
+#'      \item{geometry}{the geometry list column of edge line features}
+#'    }
+#'  }
+#'}
+#' @source \url{https://www.donneesquebec.ca/recherche/dataset/grhq}
+#' @source \url{https://www.donneesquebec.ca/recherche/dataset/structure}
+"yamaska_net"
+
+#' River line features for the Yamaska watershed
+#'
+#' An `sf` object of the `LINESTRING` geometries that make up the rivers of the
+#' Yamaska watershed of Southern Québec. The rivers make up the edges of the
+#' `yamaska_net` object. Features are projected to CRS:32198.
+#'
+#' @format An `sf` object with 620 features:
 #' \describe{
-#'   \item{geometry (edges)}{the geometry list column}
-#'   \item{pass_1 (edges)}{the passability of nodes in the network from 0 to 1}
-#'   \item{pass_2 (edges)}{the binary passability of nodes in the network, 0 if a barrier and 1 otherwise}
-#'   \item{type (edges)}{the type of the node}
-#'   \item{id (edges)}{an id column}
-#'   \item{node.label (edges)}{a logical vector representing each node's unique topoligical ID}
-#'   \item{member.label (edges)}{the segment membership of the node}
-#'   \item{from (nodes)}{the origin node of the line}
-#'   \item{to (nodes)}{the destination node of the line}
-#'   \item{OBJECTID (nodes)}{a unique river line id}
-#'   \item{geometry (nodes)}{the geometry list column}
-#'   \item{riv_length (nodes)}{the length of rivers, in meters}
-#'   \item{weight (nodes)}{a river weighting term}
-#'   \item{rivID (nodes)}{a unique river id}
-#'   ...
+#'  \item{qual}{a simulated weighting based on habitat quality of features}
+#'  \item{riv_length}{length of edge features in meters}
+#'  \item{geometry}{the geometry list column of edge line features}
 #' }
 #' @source \url{https://www.donneesquebec.ca/recherche/dataset/grhq}
 #' @source \url{https://www.donneesquebec.ca/recherche/dataset/structure}
-"yamaska"
+"yamaska_rivers"
+
+#' River line features for the Yamaska watershed
+#'
+#' An `sf` object of the `LINESTRING` geometries that make up the rivers of the
+#' Yamaska watershed of Southern Québec. The rivers make up the edges of the
+#' `yamaska_net` object. Features are projected to CRS:32198.
+#'
+#' @format An `sf` object with 620 LINESTRING features:
+#' \describe{
+#'  \item{qual}{a simulated weighting based on habitat quality of features}
+#'  \item{riv_length}{length of edge features in meters}
+#'  \item{geometry}{the geometry list column of river `LINESTRING` features}
+#' }
+#' @source \url{https://www.donneesquebec.ca/recherche/dataset/grhq}
+#' @source \url{https://www.donneesquebec.ca/recherche/dataset/structure}
+"yamaska_barriers"
+
+#' Outlet point feature for the Yamaska watershed
+#'
+#' An `sf` object of the `POINT` geometry of the outlet of the Yamaska watershed
+#' of Southern Québec. The feature is projected to CRS:32198.
+#'
+#' @format An `sf` object with 1 POINT feature:
+#' \describe{
+#'  \item{geometry}{the geometry list column of the outlet `POINT` feature}
+#' }
+#' @source \url{https://www.donneesquebec.ca/recherche/dataset/grhq}
+#' @source \url{https://www.donneesquebec.ca/recherche/dataset/structure}
+"yamaska_outlet"
