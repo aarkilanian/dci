5d425d75842004759ec1e5b6b6a5c331e6af4a7c
aarkilanian <a.arkilanian@gmail.com>
2025-05-27 13:29:19 -0230

    docs: added examples

--- R/river_net.R
+++ R/river_net.R
@@ -9,14 +9,17 @@
 #' @param barriers A `barriers` object returned by [import_points()] with
 #' `type = "bars"`.
 #' @param outlet An `outlet` object returned by [import_points()] with
-#' `type = "out"`..
+#' `type = "out"`.
 #' @param check Logical. If `TRUE` (default), dendritic topology is enforced
 #' using [enforce_dendritic()].
 #' @param tolerance A numeric value specifying the snapping distance
 #' (in map units) to align points to the river network. Defaults to `NULL`,
 #' meaning no snapping.
-#' @param max_iter Integer. Indicates the maximum number of topological correction
-#' iterations. Defaults to 10.
+#' @param max_iter An integer indicating the maximum number of correction
+#' iterations to run. As some topological errors are corrected new ones can
+#' can arise requiring multiple passes. In some cases, an automated correction
+#' choice can lead to a recursive correction that eliminates most rivers. In
+#' this case, some manual corrections may help avoid this.
 #'
 #' @return An object of class [river_net] representing the river network formed
 #' from the provided spatial inputs.
@@ -24,11 +27,20 @@
 #' @export
 #'
 #' @examples
-#' \dontrun{
-#' river_net(rivers = river_data, barriers = barrier_data, outlet = outlet_data)
-#' river_net(river_data, barrier_data, outlet_data, poi = poi_data)
-#' river_net(river_data, barrier_data, outlet_data, tolerance = 15)
-#' }
+#' # Simple case with rivers, barriers, and outlet
+#' yam_net <- river_net(rivers = yamaska_rivers, barriers = yamaska_barriers,
+#' outlet - yamaska_outlet)
+#'
+#' # If corrections are done earlier they can be skipped in this constructor
+#' riv_cor <- enforce_dendritic(rivers = yamaska_rivers, correct = TRUE,
+#' quiet = TRUE)
+#' yam_net <- river_net(rivers = yamaska_cor, barriers = yamaska_barriers,
+#' outlet - yamaska_outlet, check = FALSE)
+#'
+#' # For large river networks it may be better to specify a smaller number of
+#' # correction sweeps.
+#' yam_net <- river_net(rivers = yamaska_rivers, barriers = yamaska_barriers,
+#' outlet - yamaska_outlet, max_iter = 3)
 river_net <- function(rivers,
                       barriers,
                       outlet,
